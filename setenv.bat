588f0e094470376feb11acb3706a7479debf2cea
leviskim17 <leviskim17@gmail.com>
2020-03-22 18:58:44 +0800

    add docs

--- setenv.bat
+++ setenv.bat
@@ -1,5 +1,5 @@
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_112
-set ANT_HOME=C:\apache-ant-1.9.2
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_141
+set ANT_HOME=C:\apache-ant-1.10.1
 set JUNIT_HOME=.\jars
 
 set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
